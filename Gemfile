6db9136701668da163c22bd0d377ed0762c2d4a0
Alier <alierditmaguet@gmail.com>
2024-02-21 00:25:15 +0200

    Add devise, font-awesome-rails & letter_opener gems

--- Gemfile
+++ Gemfile
@@ -51,6 +51,7 @@ gem 'sassc-rails'
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem 'debug', platforms: %i[mri mingw x64_mingw]
+  gem 'letter_opener'
 end
 
 group :development do
@@ -69,3 +70,5 @@ gem 'bootstrap', '~> 5.3.0'
 gem 'rubocop', '>= 1.0', '< 2.0'
 
 gem 'devise'
+
+gem 'font-awesome-rails'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :update_permitted_parameters, if: :devise_controller?
+  #   def after_sign_in_path_for(_resource)
+  #     catogories_path
+  #   end
+
+  protected
+
+  def update_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:name, :email, :password, :current_password)
+    end
+  end
 end
